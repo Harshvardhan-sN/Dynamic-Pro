450f6d2fd23cfbcb3843323138edc81580c85d63
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-01-24 19:41:07 +0530

    Update dp_maxSum_nonAdjacent_striver.cpp

--- Striver-DP/dp_maxSum_nonAdjacent_striver.cpp
+++ Striver-DP/dp_maxSum_nonAdjacent_striver.cpp
@@ -2,24 +2,25 @@
 using namespace std;
 #define ll long long
 
+int dp[10007];
 
 int solve(int i,vector<int> &v1){
     if(i == 0)    return v1[i];
     if(i < 0)     return 0;
+    if(dp[i]!=-1)    return dp[i];
     int left = INT_MIN;
     int right = INT_MIN;
 
     left = solve(i-2,v1)+v1[i];
     right = solve(i-1,v1);
 
-    return max(left,right);
+    return dp[i] = max(left,right);
 }
-
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    memset(dp,-1,sizeof(dp));
     int n;      cin>>n;
     vector<int> v1;
     for(int i=0;i<n;i++){
