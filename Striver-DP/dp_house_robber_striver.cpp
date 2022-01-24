4a258289dad916bda826e02aac5f98776a3a72b6
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-01-24 20:25:23 +0530

    Update dp_house_robber_striver.cpp

--- Striver-DP/dp_house_robber_striver.cpp
+++ Striver-DP/dp_house_robber_striver.cpp
@@ -17,19 +17,38 @@ long long int solve(long long int i,vector<int> &v1,vector<long long> &dp){
     return dp[i] = max(left,right);
 }
 
+long long int second_approach(vector<long long> &v1){
+    long long int n = v1.size();
+    long long int prev = v1[0];
+    long long int prev2 = 0;
+    for(int i=1;i<n;i++){
+        long long int take = v1[i];
+        if(i>1) take+=prev2;
+        long long int notTake = prev;
+        long long int current = max(take,notTake);
+        prev2 = prev;
+        prev = current;
+    }
+    return prev;
+}
+
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    //  ******************  Input
 	long long int n;        cin>>n;
     vector<int> valueInHouse;
-
     for(int i=0;i<n;i++){
         int c;      cin>>c;
         valueInHouse.emplace_back(c);
     }
+
+    // *********************  DP array
     vector<long long> dp(n,-1),dp1(n,-1);
 
+    // **********************  first approach
     if(n==1){
         cout<<valueInHouse[0];
     }
@@ -37,9 +56,21 @@ int main(){
         long long int ans1 = solve(n-2,valueInHouse,dp);
         reverse(valueInHouse.begin(),valueInHouse.end());
         long long int ans2 = solve(n-2,valueInHouse,dp1);
-        cout<<max(ans1,ans2);
+        cout<<max(ans1,ans2)<<endl;
     }
 
+    //   *************** Second Approach *********************
+
+    vector<long long> temp1,temp2;
+    for(int i=0;i<n;i++){
+        if(i!=0)    temp1.push_back(valueInHouse[i]);
+        if(i!=n-1)    temp2.push_back(valueInHouse[i]);
+    }
+
+    if(n==1)    cout<<valueInHouse[0];
+    else{
+        cout<<max(second_approach(temp1),second_approach(temp2));
+    }
 
     return 0;
 }
